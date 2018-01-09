750ddda596902b6c2ee3569b98b5a6247f1ff1d3
ktnyt <kotone@sfc.keio.ac.jp>
2018-01-10 00:26:05 +0900

    Add base refactoring code

--- main.go
+++ main.go
@@ -2,28 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
-	"github.com/asdine/storm"
+	"github.com/boltdb/bolt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 
 	_ "github.com/joho/godotenv/autoload"
-
-	"github.com/auth0-community/go-auth0"
-	"gopkg.in/square/go-jose.v2"
 )
 
-func createMux() (*storm.DB, *echo.Echo) {
-	// Setup Storm
+func createMux() (*bolt.DB, *echo.Echo) {
+	// Setup Bolt
 	dbPath := os.Getenv("DB_PATH")
 	dbFile := fmt.Sprintf("%s/imascg.db", dbPath)
 
-	db, err := storm.Open(dbFile)
+	db, err := bolt.Open(dbFile, 0600, nil)
 
 	if err != nil {
-		fmt.Println(err)
-		panic("Storm DB could not be opened")
+		log.Fatal(err)
 	}
 
 	// Setup Echo
@@ -33,39 +30,6 @@ func createMux() (*storm.DB, *echo.Echo) {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.Use(middleware.Gzip())
-
-	e.GET("/download", func(c echo.Context) error {
-		return c.Attachment(dbFile, "imascg.db")
-	})
-
-	/// Setup Auth0 middleware
-	/// Whitelist filtering will only be active if these values are provided
-	domain := os.Getenv("AUTH0_DOMAIN")
-	client := os.Getenv("AUTH0_CLIENT")
-	secret := os.Getenv("AUTH0_SECRET")
-
-	if len(domain) > 0 && len(client) > 0 && len(secret) > 0 {
-		url := fmt.Sprintf("https://%s/", domain)
-		provider := auth0.NewKeyProvider([]byte(secret))
-		config := auth0.NewConfiguration(provider, []string{client}, url, jose.HS256)
-		validator := auth0.NewValidator(config)
-
-		/// Setup Whitelist middleware
-		allowed := func() []string {
-			editors := make([]Editor, 0)
-			if err := db.All(&editors); err != nil {
-				fmt.Println(err)
-			}
-			allowed := make([]string, len(editors))
-			for index, value := range editors {
-				allowed[index] = value.ID
-			}
-			return allowed
-		}
-
-		e.Use(Auth0(validator))
-		e.Use(Whitelist(allowed))
-	}
 
 	return db, e
 }
