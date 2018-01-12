698f4a6e5ffc5c1e7e08aa59415a42ee59032f1e
Kotone Itaya <kotone@sfc.keio.ac.jp>
2018-01-12 19:11:55 +0900

    Add units and calltable

---
+++ units.go
@@ -0,0 +1,92 @@
+package imascg
+
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/imdario/mergo"
+	"github.com/ktnyt/imascg/rest"
+	uuid "github.com/satori/go.uuid"
+)
+
+var unitNamespace = uuid.NewV5(apiNamespace, "units")
+
+// Unit is the model for units
+type Unit struct {
+	ID       string   `json:"id"`
+	Name     *string  `json:"name,omitempty"`
+	Members  []string `json:"members,omitempty"`
+	Readings []string `json:"readings,omitempty"`
+}
+
+// Validate the unit fields
+func (u *Unit) Validate() error {
+	missing := make([]string, 0)
+
+	if u.Name == nil {
+		missing = append(missing, "'name'")
+	}
+
+	if u.Members == nil {
+		missing = append(missing, "'members'")
+	}
+
+	if u.Readings == nil {
+		missing = append(missing, "'readings'")
+	}
+
+	if len(missing) > 0 {
+		return fmt.Errorf("Bad Request: %s", strings.Join(missing, ", "))
+	}
+
+	return nil
+}
+
+// MakeKey for a new unit
+func (u *Unit) MakeKey(i uint64) []byte {
+	s := strconv.FormatUint(i, 10)
+	return uuid.NewV5(unitNamespace, s).Bytes()
+}
+
+// Filter unit based on url values
+func (u *Unit) Filter(values url.Values) bool {
+	search := values.Get("search")
+
+	if len(search) > 0 {
+		for _, reading := range u.Readings {
+			if strings.Contains(reading, search) {
+				return true
+			}
+		}
+		return false
+	}
+
+	return true
+}
+
+// Merge another unit into this unit
+func (u *Unit) Merge(m rest.Model) error {
+	return mergo.MergeWithOverwrite(u, m)
+}
+
+// Clone the unit instance
+func (u *Unit) Clone() rest.Model {
+	n := *u.Name
+	m := make([]string, len(u.Members))
+	for i := range u.Members {
+		m[i] = u.Members[i]
+	}
+	r := make([]string, len(u.Readings))
+	for i := range u.Readings {
+		r[i] = u.Readings[i]
+	}
+
+	return &Unit{
+		ID:       u.ID,
+		Name:     &n,
+		Members:  m,
+		Readings: r,
+	}
+}
---
+++ vars.go
@@ -0,0 +1,5 @@
+package imascg
+
+import uuid "github.com/satori/go.uuid"
+
+var apiNamespace = uuid.NewV5(uuid.NamespaceURL, "api.imascg.moe")
