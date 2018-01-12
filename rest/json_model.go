c2027c10a302073115b1542c694ff612a652ca08
ktnyt <kotone@sfc.keio.ac.jp>
2018-01-13 00:32:38 +0900

    Fix project structure

---
+++ rest/json_model.go
@@ -0,0 +1,40 @@
+package rest
+
+import "encoding/json"
+
+// NewJSONModel returns a new jsonMarshalable MarshalableModel
+func NewJSONModel(model Model) MarshalableModel {
+	return MarshalableModel{m: &jsonMarshalable{model: model}}
+}
+
+type jsonMarshalable struct {
+	model Model
+}
+
+func (j *jsonMarshalable) GetModel() Model {
+	return j.model
+}
+
+func (j *jsonMarshalable) SetModel(m Model) {
+	j.model = m
+}
+
+func (j *jsonMarshalable) Marshal() ([]byte, error) {
+	return json.Marshal(&j.model)
+}
+
+func (j *jsonMarshalable) Unmarshal(data []byte) error {
+	return json.Unmarshal(data, &j.model)
+}
+
+func (j *jsonMarshalable) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&j.model)
+}
+
+func (j *jsonMarshalable) UnmarshalJSON(data []byte) error {
+	return json.Unmarshal(data, &j.model)
+}
+
+func (j *jsonMarshalable) Clone() Marshalable {
+	return &jsonMarshalable{model: j.model.Clone()}
+}
---
+++ rest/model.go
@@ -0,0 +1,73 @@
+package rest
+
+import "net/url"
+
+// Model defines the interface required for REST handling
+type Model interface {
+	Validate() error
+	MakeKey(uint64) []byte
+	Filter(url.Values) bool
+	Merge(Model) error
+	Clone() Model
+}
+
+// Marshalable defines the interface required for Model marshalling
+type Marshalable interface {
+	GetModel() Model
+	SetModel(Model)
+	Marshal() ([]byte, error)
+	Unmarshal([]byte) error
+	MarshalJSON() ([]byte, error)
+	UnmarshalJSON([]byte) error
+	Clone() Marshalable
+}
+
+// MarshalableModel is the integrated type for Model and Marshalable
+type MarshalableModel struct {
+	m Marshalable
+}
+
+// Validate delegates the call to Marshalable.Model.Validate
+func (m *MarshalableModel) Validate() error {
+	return m.m.GetModel().Validate()
+}
+
+// MakeKey delegates the call to Marshalable.Model.MakeKey
+func (m *MarshalableModel) MakeKey(n uint64) []byte {
+	return m.m.GetModel().MakeKey(n)
+}
+
+// Filter delegates the call to Marshalable.Model.Filter
+func (m *MarshalableModel) Filter(v url.Values) bool {
+	return m.m.GetModel().Filter(v)
+}
+
+// Merge delegates the call to Marshalable.Model.Merge
+func (m *MarshalableModel) Merge(other MarshalableModel) error {
+	return m.m.GetModel().Merge(other.m.GetModel())
+}
+
+// Marshal delegates the call to Marshalable.Marshal
+func (m *MarshalableModel) Marshal() ([]byte, error) {
+	return m.m.Marshal()
+}
+
+// Unmarshal delegates the call to Marshalable.Unmarshal
+func (m *MarshalableModel) Unmarshal(data []byte) error {
+	return m.m.Unmarshal(data)
+}
+
+// MarshalJSON delegates the call to Marshalable.MarshalJSON
+func (m *MarshalableModel) MarshalJSON() ([]byte, error) {
+	return m.m.MarshalJSON()
+}
+
+// UnmarshalJSON delegates the call to Marshalable.UnmarshalJSON
+func (m *MarshalableModel) UnmarshalJSON(data []byte) error {
+	return m.m.UnmarshalJSON(data)
+}
+
+// Clone delegates the call to Marshalable.Clone
+func (m *MarshalableModel) Clone() MarshalableModel {
+	return MarshalableModel{m: m.m.Clone()}
+}
