3fa57f4e9986e81e74c708d3f80ad7568048bf8c
Kotone Itaya <kotone@sfc.keio.ac.jp>
2018-01-12 19:11:55 +0900

    Add units and calltable

--- cmd/imascg/calltable.go
+++ cmd/imascg/calltable.go
@@ -8,11 +8,11 @@ import (
 )
 
 func init() {
-	m := rest.NewJSONModel(&imascg.Character{})
-	h, err := rest.NewBoltHandler(db, []byte("characters"), m)
+	m := rest.NewJSONModel(&imascg.Calltable{})
+	h, err := rest.NewBoltHandler(db, []byte("calltable"), m)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	rest.Register(h, e.Group("characters"))
+	rest.Register(h, e.Group("calltable"))
 }
